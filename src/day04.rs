f987d409c8db510e6bb638e4b3456e7f5dffe7d5
Renee Gyllensvaan <renee.gyllensvaan@factset.com>
2023-12-04 09:44:55 +0000

    day04

---
+++ src/day04.rs
@@ -0,0 +1,115 @@
+fn part1(input: &str) -> i64 {
+    let mut result: i64 = 0;
+
+    for line in input.lines() {
+        let (card_id, rest) = line.split_once(':').unwrap();
+        let _card_id: i64 = card_id
+            .split_whitespace()
+            .skip(1)
+            .next()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        let (numbers, guesses) = rest.split_once('|').unwrap();
+        let numbers: Vec<i64> = numbers
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect();
+        let guesses: Vec<i64> = guesses
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect();
+
+        let mut line_score = 0;
+        for number in &numbers {
+            for guess in &guesses {
+                if number == guess {
+                    line_score = 1.max(line_score * 2);
+                    break;
+                }
+            }
+        }
+        result += line_score;
+    }
+
+    result
+}
+
+fn part2(input: &str) -> i64 {
+    let mut result: i64 = 0;
+
+    let mut copies = Vec::<usize>::new();
+    for (ix, line) in input.lines().enumerate() {
+        let (_card_id, rest) = line.split_once(':').unwrap();
+
+        let (numbers, guesses) = rest.split_once('|').unwrap();
+        let numbers: Vec<i64> = numbers
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect();
+        let guesses: Vec<i64> = guesses
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect();
+
+        let num_copies = 1 + copies.iter().filter(|v| ix <= **v).count();
+        for _ in 0..num_copies {
+            result += 1;
+            let mut line_matches = 0;
+            for number in &numbers {
+                for guess in &guesses {
+                    if number == guess {
+                        line_matches += 1;
+                        break;
+                    }
+                }
+            }
+            if line_matches > 0 {
+                copies.push(ix + line_matches);
+            }
+        }
+    }
+
+    result
+}
+
+pub fn main() {
+    let input = std::fs::read_to_string("input/day04").unwrap();
+    println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
+}
+
+#[test]
+fn test_part1_example() {
+    let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
+    assert_eq!(part1(input), 13);
+}
+
+#[test]
+fn test_part1_facit() {
+    let input = std::fs::read_to_string("input/day04").unwrap();
+    assert_eq!(part1(&input), 27059);
+}
+
+#[test]
+fn test_part2_example() {
+    let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
+    assert_eq!(part2(input), 30);
+}
+
+#[test]
+fn test_part2_facit() {
+    let input = std::fs::read_to_string("input/day04").unwrap();
+    assert_eq!(part2(&input), 5744979);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 fn main() {
     // day01::main();
     // day02::main();
-    day03::main();
+    // day03::main();
+    day04::main();
 }
